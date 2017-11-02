c0fd96debeaa009b6044b6b8840347dce60d2830
Carlos <thehandyowl.io@gmail.com>
2017-11-02 18:37:44 +0100

    Renombrando controlador y vista

--- app/src/main/java/com/tho/guedragain/ForecastActivity.kt
+++ app/src/main/java/com/tho/guedragain/ForecastActivity.kt
@@ -3,13 +3,13 @@ package com.tho.guedragain
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ForecastActivity : AppCompatActivity() {
 
-    val TAG = MainActivity::class.java.canonicalName
+    val TAG = ForecastActivity::class.java.canonicalName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
 
     }
 
