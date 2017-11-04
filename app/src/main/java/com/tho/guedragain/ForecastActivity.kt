1989f2701f19ac66978fd77637f50653be92fafa
Carlos <thehandyowl.io@gmail.com>
2017-11-04 15:16:33 +0100

    Creamos el setter personalizado, que actualizará el atributo y borramos el método update

--- app/src/main/java/com/tho/guedragain/ForecastActivity.kt
+++ app/src/main/java/com/tho/guedragain/ForecastActivity.kt
@@ -8,33 +8,32 @@ import android.widget.TextView
 class ForecastActivity : AppCompatActivity() {
 
     var forecast: Forecast? = null
+        set(value) {
+            // Accedemos a las vistas de la interfaz
+            val forecastImage = findViewById<ImageView>(R.id.forecast_image)
+            val maxTemp = findViewById<TextView>(R.id.max_temp)
+            val minTemp = findViewById<TextView>(R.id.min_temp)
+            val humidity = findViewById<TextView>(R.id.humidity)
+            val forecastDescription = findViewById<TextView>(R.id.forecast_description)
+
+            // Actualizamos la visa con el modelo
+            if(forecast != null) {
+                forecastImage.setImageResource(forecast.icon)
+                forecastDescription.text = forecast.description
+                val maxTempString = getString(R.string.max_temp_format, forecast.maxTemp)
+                val minTempString = getString(R.string.min_temp_format, forecast.minTemp)
+                val humidityString = getString(R.string.humidity_format, forecast.humidity)
+                maxTemp.text = maxTempString
+                minTemp.text = minTempString
+                humidity.text = humidityString
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
         forecast = Forecast(25f, 10f, 35f, "Soleado con alguna nube", R.drawable.ico_01)
-
-        updateForecast()
-    }
-
-    private fun updateForecast() {
-        // Accedemos a las vistas de la interfaz
-        val forecastImage = findViewById<ImageView>(R.id.forecast_image)
-        val maxTemp = findViewById<TextView>(R.id.max_temp)
-        val minTemp = findViewById<TextView>(R.id.min_temp)
-        val humidity = findViewById<TextView>(R.id.humidity)
-        val forecastDescription = findViewById<TextView>(R.id.forecast_description)
-
-        // Actualizamos la visa con el modelo
-        forecastImage.setImageResource(forecast.icon)
-        forecastDescription.text = forecast.description
-        val maxTempString = getString(R.string.max_temp_format, forecast.maxTemp)
-        val minTempString = getString(R.string.min_temp_format, forecast.minTemp)
-        val humidityString = getString(R.string.humidity_format, forecast.humidity)
-        maxTemp.text = maxTempString
-        minTemp.text = minTempString
-        humidity.text = humidityString
     }
 
 }
