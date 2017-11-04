5157584407deb6a39772525e2fac4851c2929cea
Carlos <thehandyowl.io@gmail.com>
2017-11-04 19:06:10 +0100

    Preparando el listener de aceptar

--- app/src/main/java/com/tho/guedragain/SettingsActivity.kt
+++ app/src/main/java/com/tho/guedragain/SettingsActivity.kt
@@ -2,10 +2,17 @@ package com.tho.guedragain
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 
 class SettingsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        findViewById<View>(R.id.ok_btn).setOnClickListener { acceptSettings() }
     }
+
+    private fun acceptSettings() { }
+    
 }
