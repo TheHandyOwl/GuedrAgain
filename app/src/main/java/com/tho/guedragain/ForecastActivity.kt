798e863002679eab61ba0b5afc4a06446883febc
Carlos <thehandyowl.io@gmail.com>
2017-11-04 13:45:27 +0100

    Empezamos a actualizar la vista pero faltan cadenas con formato para pasar valores

--- app/src/main/java/com/tho/guedragain/ForecastActivity.kt
+++ app/src/main/java/com/tho/guedragain/ForecastActivity.kt
@@ -2,6 +2,8 @@ package com.tho.guedragain
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 
 class ForecastActivity : AppCompatActivity() {
 
@@ -17,6 +19,14 @@ class ForecastActivity : AppCompatActivity() {
     }
 
     private fun setForecast(forecast: Forecast) {
+        // Accedemos a las vistas de la interfaz
+        val forecastImage = findViewById<ImageView>(R.id.forecast_image)
+        val maxTemp = findViewById<TextView>(R.id.max_temp)
+        val minTemp = findViewById<TextView>(R.id.min_temp)
+        val humidity = findViewById<TextView>(R.id.humidity)
+        val forecastDescription = findViewById<TextView>(R.id.forecast_description)
+
+        // Actualizamos la visa con el modelo
 
     }
 
