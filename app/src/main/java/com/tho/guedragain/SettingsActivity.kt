99cc509c379e501531d31790b3c0918ba1dfe069
Carlos <thehandyowl.io@gmail.com>
2017-11-04 19:22:57 +0100

    Añadimos la referencia al RadioGroup y lo iniciamos

--- app/src/main/java/com/tho/guedragain/SettingsActivity.kt
+++ app/src/main/java/com/tho/guedragain/SettingsActivity.kt
@@ -3,15 +3,20 @@ package com.tho.guedragain
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
+import android.widget.RadioGroup
 
 class SettingsActivity: AppCompatActivity() {
+
+    var radioGroup: RadioGroup? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         findViewById<View>(R.id.ok_btn).setOnClickListener { acceptSettings() }
         findViewById<View>(R.id.cancel_btn).setOnClickListener { cancelSettings() }
+
+        radioGroup = findViewById(R.id.units_rg)
 
     }
 
