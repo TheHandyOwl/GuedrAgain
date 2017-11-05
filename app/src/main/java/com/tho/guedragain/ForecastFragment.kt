9ca90df6152f0c5c31fa498fc5e88b56b0d524dd
Carlos <thehandyowl.io@gmail.com>
2017-11-05 20:31:35 +0100

    Creando ForecastFragment e implementando onCreateView

---
+++ app/src/main/java/com/tho/guedragain/ForecastFragment.kt
@@ -0,0 +1,13 @@
+package com.tho.guedragain
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ForecastFragment: Fragment()  {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
